3cffdb992914f23d6b8c0b3e91806ba92f701ea8
Ruja Romulus-Emanuel <romulus-emanuel.ruja@tutanota.com>
2020-09-13 18:32:22 +0300

    ..

--- src/subdir.mk
+++ src/subdir.mk
@@ -1,22 +1,22 @@
-OBJECTS += ./src/rsa_pem.o\
+OBJECTS += ./src/aes.o\
 ./src/sha256.o\
-./src/aes_init.o\
-./src/hash.o\
+./src/rsa_pem.o\
 ./src/init.o\
-./src/base64.o\
-./src/rsa_sign.o\
 ./src/rsa.o\
-./src/aes.o
+./src/hash.o\
+./src/base64.o\
+./src/aes_init.o\
+./src/rsa_sign.o
 
-CC_DEPS += ./src/deps/rsa_pem.d\
+CC_DEPS += ./src/deps/aes.d\
 ./src/deps/sha256.d\
-./src/deps/aes_init.d\
-./src/deps/hash.d\
+./src/deps/rsa_pem.d\
 ./src/deps/init.d\
-./src/deps/base64.d\
-./src/deps/rsa_sign.d\
 ./src/deps/rsa.d\
-./src/deps/aes.d
+./src/deps/hash.d\
+./src/deps/base64.d\
+./src/deps/aes_init.d\
+./src/deps/rsa_sign.d
 
 ./src/%.o: ./src/%.cc
 	$(CC) -Wall -c -fPIC $< -o $@
