c80cdb3e5840b1bec5d72c059a8ef57ecec022cd
Ruja Romulus-Emanuel <romulus-emanuel.ruja@tutanota.com>
2020-09-14 16:54:04 +0300

    version v0.3.1

--- src/subdir.mk
+++ src/subdir.mk
@@ -1,28 +1,6 @@
-<<<<<<< HEAD
-OBJECTS += ./src/aes.o\
-./src/sha256.o\
-./src/rsa_pem.o\
-./src/init.o\
-./src/rsa.o\
-./src/hash.o\
-./src/base64.o\
-./src/aes_init.o\
-./src/rsa_sign.o
-
-CC_DEPS += ./src/deps/aes.d\
-./src/deps/sha256.d\
-./src/deps/rsa_pem.d\
-./src/deps/init.d\
-./src/deps/rsa.d\
-./src/deps/hash.d\
-./src/deps/base64.d\
-./src/deps/aes_init.d\
-./src/deps/rsa_sign.d
-=======
 OBJECTS += ./src/hash.o
 
 CC_DEPS += ./src/deps/hash.d
->>>>>>> new_structure
 
 ./src/%.o: ./src/%.cc
 	$(CC) -Wall -c -fPIC $< -o $@
--- src/v1/subdir.mk
+++ src/v1/subdir.mk
@@ -1,14 +1,14 @@
 OBJECTS += ./src/v1/sha.o\
-./src/v1/base64.o\
 ./src/v1/init.o\
-./src/v1/rsa.o\
-./src/v1/aes.o
+./src/v1/aes.o\
+./src/v1/base64.o\
+./src/v1/rsa.o
 
 CC_DEPS += ./src/v1/deps/sha.d\
-./src/v1/deps/base64.d\
 ./src/v1/deps/init.d\
-./src/v1/deps/rsa.d\
-./src/v1/deps/aes.d
+./src/v1/deps/aes.d\
+./src/v1/deps/base64.d\
+./src/v1/deps/rsa.d
 
 ./src/v1/%.o: ./src/v1/%.cc
 	$(CC) -Wall -c -fPIC $< -o $@
